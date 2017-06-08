b4dfe1b535bcb463f4259f5962e40f61a39c0bb8
Elana Celano <elanacelano@gmail.com>
2017-06-08 13:55:07 -0400

    First Commit

---
+++ keys.js
@@ -0,0 +1,34 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: 'FALIJ7GWqcIAGxnEmkCCr7aZW',
+  consumer_secret: 'NqAI6uRgH7czJpgTMlldS6kJvCwtLi70GR1Esgk8mg6QhChb6n',
+  access_token_key: '872821784239886336-xLD3BKyJxF39yEmlpE0V5anm3R2frMS',
+  access_token_secret: 'cxgTdc4ynwd3VICBdfNR9nA5MvfuevpN99DpY2QDVtJKi',
+});
+
+
+	// for Twitter api key (?)
+
+// Consumer Key (API Key)	FALIJ7GWqcIAGxnEmkCCr7aZW
+// Consumer Secret (API Secret)	NqAI6uRgH7czJpgTMlldS6kJvCwtLi70GR1Esgk8mg6QhChb6n
+// Access Level	Read and write (modify app permissions)
+// Owner	BehrGirl
+// Owner ID	872821784239886336
+
+// Access Token	872821784239886336-xLD3BKyJxF39yEmlpE0V5anm3R2frMS
+// Access Token Secret	cxgTdc4ynwd3VICBdfNR9nA5MvfuevpN99DpY2QDVtJKi
+
+
+// 3. Here we put into action what we pieced together in step 1 and 2. See client.get (that's the same client we created in step 2.). All you need to do now is replace the word 'nodejs' with
+// your twitter handle or someone elses twitter handle.
+
+var params = {screen_name: 'BehrGirl'};
+
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
+
+
---
+++ liri.js
@@ -0,0 +1,11 @@
+var data = {
+	my-tweets: "",
+	spotify-this-song,"I Want it That Way",
+	movie-this: "",
+	do-what-it-says: "",
+	makeNoise: function () {
+		if (this.data === true){
+		console.log("WooHoo!");	
+		}
+	}
+};
