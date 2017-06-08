7307a3cf9bf74c9852ce7a51cb700f03c551bd58
Elana Celano <elanacelano@gmail.com>
2017-06-08 14:23:00 -0400

    Second Commit

--- liri.js
+++ liri.js
@@ -1,11 +1,17 @@
 var data = {
-	my-tweets: "",
-	spotify-this-song,"I Want it That Way",
-	movie-this: "",
-	do-what-it-says: "",
-	makeNoise: function () {
-		if (this.data === true){
+	my-tweets = "";
+	spotify-this-song ="<I Want it That Way>";
+	movie-this = "<>";
+	do-what-it-says = "<>";
+		
+		function () {
+		// if (this.data === true){
 		console.log("WooHoo!");	
 		}
 	}
 };
+
+data.my-tweets();
+data.spotify-this-song();
+data.movie-this();
+data.do-what-it-says();
